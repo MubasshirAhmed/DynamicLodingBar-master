ec2914c7762b54cec16ea59434f282e7fdea397a
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-08-15 15:20:03 +0600

    first commit

---
+++ DynamicLodingBar/Assets/LoadLevelScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevelScript : MonoBehaviour
+{
+    public GameObject LoadingUIBG;
+    public Slider slider;
+    public Text loadingText;
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsysnchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsysnchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        LoadingUIBG.SetActive(true);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            loadingText.text = (progress * 100).ToString();
+            yield return null;
+        }
+    }
+
+}
